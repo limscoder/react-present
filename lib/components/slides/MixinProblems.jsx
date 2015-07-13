22926c46ec1dc197b6fb4c7b1eb4d5ae8c8fbda0
Dave Thompson <dthompson@dthompson-mbp.f4tech.com>
2015-07-13 11:41:46 -0700

    Updated slide text.

--- lib/components/slides/MixinProblems.jsx
+++ lib/components/slides/MixinProblems.jsx
@@ -12,8 +12,8 @@ export default React.createClass({
         <br />
 
         <ul>
-          <li>Mixins don't work with ES6 classes</li>
-          <li>What if you need <code>shouldComponentUpdate</code>?</li>
+          <li>Mixins don't work with ES6 classes.</li>
+          <li>One-per-component lifecycle method conflicts.</li>
           <li><code>this.setState()</code> outside of component can be confusing.</li>
         </ul>
       </div>
