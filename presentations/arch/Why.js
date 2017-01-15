c07edc077e65fe03467cfbe54eeb0ca3a260a5b8
Muneeb Baderoen <mbaderoen@gmail.com>
2017-01-16 00:29:05 +0200

    Fix hot-reload

--- presentations/arch/Why.js
+++ presentations/arch/Why.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ListBlock from 'components/ListBlock';
 
-const listItems = [
-  '?',
-];
-
 export default class Slide extends React.Component {
   render() {
     return (
       <div>
-        <ListBlock title="What" items={ listItems }/>
+        <h1>Why</h1>
       </div>
     );
   }
