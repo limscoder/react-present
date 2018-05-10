59012e61a47fba006e3bb51d9fa9c9217eca9c9e
limscoder <dthomp325@gmail.com>
2018-05-10 17:10:33 -0600

    Added mha slides.

--- presentations/mile-high-agile-2018/slideList.js
+++ presentations/mile-high-agile-2018/slideList.js
@@ -31,6 +31,11 @@ import spikeSrc from './images/spike.png'
 import improveSrc from './images/improve.png'
 import integrateSrc from './images/integrate.png'
 import gavelSrc from './images/gavel.png'
+import pieSrc from './images/pie.png'
+import teslaSrc from './images/tesla.png'
+import surveySrc from './images/devsurvey.png'
+import planningSrc from './images/planning.png'
+import testSrc from './images/tests.png'
 
 const flowCode =
 `// @flow
@@ -234,21 +239,95 @@ export default {
       <Title>Case Study: Flowtype</Title>
       <Code>{ flowCode }</Code>
     </Slide>,
-    // TODO dealing with conflict!
     <Slide>
-      <Title>Making the decision</Title>
+      <Title>Making contentious decisions</Title>
       <img src={ gavelSrc } alt="gavel" />
     </Slide>,
-    // --- conflict
-  
-
-    // TODO dev tools
-    // TODO dev ops
-    // TODO ops
-    // TODO planning
-    
     <Slide>
-      <Statement>Bring intelligent architecture to your organization</Statement>
+      <Title>Consensus breaks down</Title>
+      <img src={ pieSrc } alt="pie" />
+    </Slide>,
+    <Slide>
+      <Title>Get personal</Title>
+      <Statement>Teams and 1 - 1</Statement>
+    </Slide>,
+    <Slide>
+      <Title>Explain the decision</Title>
+      <Statement>Present from authority</Statement>
+      <Statement>Relate to larger goals</Statement>
+      <Statement>Be specific</Statement>
+      <Statement>Respond to questions and feedback</Statement>
+    </Slide>,
+    <Slide>
+      <Title>Ensure r + d morale</Title>
+      <Statement>Everyone wants to get features shipped</Statement>
+      <Statement>Devs want to work on career relevant tech</Statement>
+    </Slide>,
+    <Slide>
+      <Title>Frustrations</Title>
+      <Statement>Tech debt + legacy tech</Statement>
+      <Statement>Long lead times</Statement>
+      <Statement>Cumbersome or slow tooling</Statement>
+    </Slide>,
+    <Slide>
+      <Title>Support the developer environment</Title>
+      <img src={ teslaSrc } alt="tesla" />
+    </Slide>,
+    <Slide>
+      <Title>Larger issues</Title>
+      <Statement>Business wants to know roi</Statement>
+      <Delay>
+        <img src={ surveySrc } alt="survey" />
+      </Delay>
+    </Slide>,
+    <Slide>
+      <Title>Plan it!</Title>
+      <Statement>Architecture team plans techdebt work</Statement>
+      <img src={ planningSrc } alt="planning board" />
+    </Slide>,
+    <Slide>
+      <Title>Case Study: too many tests</Title>
+      <img src={ testSrc } alt="tests" />
+    </Slide>,
+    <Slide>
+      <Title>Measuring velocity</Title>
+      <Statement>completed work / time increment</Statement>
+    </Slide>,
+    <Slide>
+      <Title>DevOps</Title>
+      <Statement>Development teams are responsible for when and how their features are deployed to customers.</Statement>
+    </Slide>,
+    <Slide>
+      <Title>DevOps drives agile transformations</Title>
+    </Slide>,
+    <Slide>
+      <Title>Ensure rapid and safe deployments</Title>
+      <Statement>Rapid deployments reduce lead time</Statement>
+      <Statement>Safety improves user experience</Statement>
+      <Statement>Robust + repeatable CI/CD > more automation</Statement>
+    </Slide>,
+    <Slide>
+      <Title>Culture of responsibility</Title>
+      <Statement>Enable teams to observe their deployment health</Statement>
+      <img src={ ftSrc } alt="freshtracks" />
+    </Slide>,
+    <Slide>
+      <Title>When deployments break</Title>
+      <Statement>Plan for quick reaction</Statement>
+      <Statement>Seek process and technical improvements instead of blame</Statement>
+      <Statement>Identify what went right the other times</Statement>
+    </Slide>,
+    <Slide>
+      <Title>Case Study: recent outage</Title>
+      <Delay>
+        <Statement>Notified by alerts</Statement>
+        <Statement>Used metrics to evaluate severity</Statement>
+        <Statement>Debugged and remedied issue</Statement>
+        <Statement>Retro: tests and protections for high customer load</Statement>
+      </Delay>
+    </Slide>,
+    <Slide>
+      <Title>Bring intelligent architecture to your organization</Title>
       <Delay>
         <Statement>Start small</Statement>
         <Statement>Establish communication channels</Statement>
